29e87c82909dff45fbc3e981577560804d3149d6
syed mustafa shiraz <syedmustafa205@gmail.com>
2023-06-27 09:06:03 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import './App.css';
+//import {BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import FooterComponent from './components/FooterComponent';
+import HeaderComponent from './components/HeaderComponent';
+import ListEmployeeComponent from './components/ListEmployeeComponent';
+import AddEmployeeComponent from './components/AddEmployeeComponent';
+
+function App() {
+  return (
+    <div>
+     
+        <HeaderComponent />
+        <div className= "container">
+          <Routes>
+             
+              <Route exact path="/" element = {<ListEmployeeComponent/>}></Route>
+              <Route path = "/employees" element = {<ListEmployeeComponent/>}></Route>
+              <Route path = "/add-employee" element = {<AddEmployeeComponent/>} ></Route>
+              <Route path = "/edit-employee/:id" element = {<AddEmployeeComponent/>}></Route>
+            </Routes>
+        </div>
+        <FooterComponent />
+      
+    </div>
+  );
+}
+
+export default App;
